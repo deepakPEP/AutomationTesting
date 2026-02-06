01b2f41631a2cfdea1eca8dbffe80b32e071ba19
Your Name <anusuya.m@pepagora.org>
2026-02-06 11:51:41 +0530

    Revert "Merge pull request #32 from deepakPEP/Jan28Branch"
    This reverts commit 3f1db104f853334be54dfad205d407ccd999e78d, reversing
    changes made to a1d0c3d82f8e5e8a5fa19fc60fa91a4b0c2d69a4.

--- src/tests/Mails/example.spec.ts
+++
@@ -1,7 +0,0 @@
-  const { readLatestEmail } = require('./Mailtest');
-  import { test, expect } from '@playwright/test';
-
-  test('@product_I_sell_without_variants', async () => {
-    const email = await readLatestEmail();
-    expect(email.subject).toContain('Welcome');
-  });
